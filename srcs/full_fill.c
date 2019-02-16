35a689887ef98d33d2a873b0de079c385b098b3e
akeiflin <akeiflin@akeiflins-iMac.local>
2019-02-15 16:30:02 -0800

    modification des ACL

--- srcs/full_fill.c
+++ srcs/full_fill.c
@@ -6,7 +6,7 @@
 /*   By: akeiflin <akeiflin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 17:58:30 by akeiflin          #+#    #+#             */
-/*   Updated: 2019/02/11 20:11:45 by akeiflin         ###   ########.fr       */
+/*   Updated: 2019/02/15 12:16:48 by akeiflin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,39 +52,71 @@ char		*ft_lasttime(time_t filetime)
 
 char		*ft_owner(uid_t st_uid)
 {
-	struct passwd *pwd;
+	struct passwd	*pwd;
+	char			*id;
 
 	pwd = getpwuid(st_uid);
-	return (pwd->pw_name);
+	if (pwd)
+		id = pwd->pw_name;
+	else
+		id = ft_itoa(st_uid);
+	return (id);
 }
 
 char		*ft_group(gid_t st_gid)
 {
-	struct group *gr;
+	struct group	*gr;
+	char			*id;
 
 	gr = getgrgid(st_gid);
+	if (gr)
+		id = gr->gr_name;
+	else
+		id = ft_itoa(st_gid);
 	return (gr->gr_name);
 }
 
 //A FREE
 // Mauvaise size d'alloc ATTENTION
 
+char		*ft_set_type(mode_t st_mode)
+{
+	char	*str;
+
+	str = NULL;
+	if (S_ISDIR(st_mode))
+		str = ft_strjoin(str, "d");
+	else if (S_ISCHR(st_mode))
+		str = ft_strjoin(str, "c");
+	else if (S_ISBLK(st_mode))
+		str = ft_strjoin(str, "b");
+	else if (S_ISFIFO(st_mode))
+		str = ft_strjoin(str, "p");
+	else if (S_ISSOCK(st_mode))
+		str = ft_strjoin(str, "s");
+	else if (S_ISLNK(st_mode))
+		str = ft_strjoin(str, "l");
+	else
+		str = ft_strjoin(str, "-");
+	return (str);
+}
+
 char		*ft_perm(mode_t st_mode)
 {
 	char	*str;
 
-	str = malloc(sizeof(str) * 11);
-	*(str++) = ((S_ISDIR(st_mode)) ? 'd' : '-');
-	*(str++) = ((st_mode & S_IRUSR) ? 'r' : '-');
-	*(str++) = ((st_mode & S_IWUSR) ? 'w' : '-');
-	*(str++) = ((st_mode & S_IXUSR) ? 'x' : '-');
-	*(str++) = ((st_mode & S_IRGRP) ? 'r' : '-');
-	*(str++) = ((st_mode & S_IWGRP) ? 'w' : '-');
-	*(str++) = ((st_mode & S_IXGRP) ? 'x' : '-');
-	*(str++) = ((st_mode & S_IROTH) ? 'r' : '-');
-	*(str++) = ((st_mode & S_IWOTH) ? 'w' : '-');
-	*(str) = ((st_mode & S_IXOTH) ? 'x' : '-');
-	return (str - 9);
+	str = NULL;
+	str = ft_set_type(st_mode);
+	str = ft_strljoin(str, ((st_mode & S_IRUSR) ? "r" : "-"), FIRST);
+	str = ft_strljoin(str, ((st_mode & S_IWUSR) ? "w" : "-"), FIRST);
+	str = ft_strljoin(str, ((st_mode & S_IXUSR) ? "x" : "-"), FIRST);
+	str = ft_strljoin(str, ((st_mode & S_IRGRP) ? "r" : "-"), FIRST);
+	str = ft_strljoin(str, ((st_mode & S_IWGRP) ? "w" : "-"), FIRST);
+	str = ft_strljoin(str, ((st_mode & S_IXGRP) ? "x" : "-"), FIRST);
+	str = ft_strljoin(str, ((st_mode & S_IROTH) ? "r" : "-"), FIRST);
+	str = ft_strljoin(str, ((st_mode & S_IWOTH) ? "w" : "-"), FIRST);
+	str = ft_strljoin(str, ((st_mode & S_IXOTH) ? "x" : "-"), FIRST);
+	return (str);
 }
 
 // A refaire et comprendre
